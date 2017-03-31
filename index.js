2b21e9c44df0f5c3be6bf14a524d8e51600c5594
Mohamed Elkady <mohamedelkady@gmail.com>
2017-03-31 16:19:12 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+"use strict";
+
+const request = require('request')
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+const token = process.env.TOKEN
+
+app.get('/token', function (req, res) {
+  res.send('Hello, token is in the URL now!')
+})
+
+app.get('/feed', function (req, res) {
+	res.set('Content-Type', 'application/json');
+  	request(
+	    { method: 'GET'
+	    , uri: 'https://api.instagram.com/v1/users/google/media/recent/?access_token=' + token
+	    , gzip: true
+	    }
+  	, function (error, response, body) {
+  		if(!error) {
+  			return res.status(500).send(response.body)
+  		}
+
+    	res.status(200).send(response);
+    }
+  )
+})
+
+app.listen(port, function () {
+  console.log('App listening on port 3000!')
+})
